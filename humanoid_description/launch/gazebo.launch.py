89499f8a663b6371b01708ecabb58a5f3eeccfad
Thushar Kumar.G.A <thusharkumarga@gmail.com>
2025-02-21 13:47:09 +0530

    Updated package

--- humanoid_description/launch/gazebo.launch.py
+++ humanoid_description/launch/gazebo.launch.py
@@ -12,7 +12,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('humanoid_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'humanoid.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'humanoid_control.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
@@ -63,30 +63,6 @@ def generate_launch_description():
         ],
         output='screen'
     )
-    # ros2_control integration
-    config_file_name = 'humanoid_controllers.yaml'
-    config = os.path.join(share_dir, 'config', config_file_name)
-
-    controller_manager_node = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
-        parameters=[config],
-        output='screen'
-    )
-
-    robot_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['robot_controller'],
-        output='screen'
-    )
-
-    joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
-        output='screen'
-    )
 
     return LaunchDescription([
         robot_state_publisher_node,
@@ -94,7 +70,4 @@ def generate_launch_description():
         gazebo_server,
         gazebo_client,
         urdf_spawn_node,
-        controller_manager_node,
-        joint_state_broadcaster_spawner,
-        robot_controller_spawner,
     ])
